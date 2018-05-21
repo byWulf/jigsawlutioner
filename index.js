e7d2dc79030a9ffe9bd409497cb1e9d34e4efdce
byWulf <wulf1337@gmail.com>
2018-05-21 13:44:29 +0200

    Added PieceHelper to exports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const BorderFinder = require('./src/borderFinder');
 const SideFinder = require('./src/sideFinder');
 const Matcher = require('./src/matcher');
+const PieceHelper = require('./src/pieceHelper');
 
 module.exports = {
     BorderFinder: BorderFinder,
     SideFinder: SideFinder,
     Matcher: Matcher,
-    Debug: Debug
+    Debug: Debug,
+    PieceHelper: PieceHelper
 };
