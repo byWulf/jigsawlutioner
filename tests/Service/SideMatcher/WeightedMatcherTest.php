2ca7e9c3ffd386f042d01f2e2dc8b66e286040a3
Michael Wolf <michael.wolf@check24.de>
2021-12-18 09:16:01 +0100

    Fix unittests

--- tests/Service/SideMatcher/WeightedMatcherTest.php
+++ tests/Service/SideMatcher/WeightedMatcherTest.php
@@ -7,12 +7,10 @@ namespace Bywulf\Jigsawlutioner\Tests\Service\SideMatcher;
 use Bywulf\Jigsawlutioner\Dto\Piece;
 use Bywulf\Jigsawlutioner\Dto\Side;
 use Bywulf\Jigsawlutioner\Service\SideMatcher\WeightedMatcher;
-use Bywulf\Jigsawlutioner\SideClassifier\DepthClassifier;
 use Bywulf\Jigsawlutioner\Tests\PieceLoaderTrait;
 use PHPStan\Testing\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
 
-// TODO: Fix unittests
 class WeightedMatcherTest extends TestCase
 {
     use ProphecyTrait;
@@ -25,41 +23,6 @@ class WeightedMatcherTest extends TestCase
         $this->service = new WeightedMatcher();
     }
 
-    public function testGetMatchingProbability(): void
-    {
-        $pieces = $this->getPieces();
-
-        for ($x = 0; $x < 25; ++$x) {
-            for ($y = 0; $y < 20; ++$y) {
-                $rightSide = $pieces[$y * 25 + $x + 2]->getSides()[3] ?? null;
-                $rightOppositeSide = $x === 24 ? null : ($pieces[$y * 25 + $x + 3]->getSides()[1] ?? null);
-
-                $bottomSide = $pieces[$y * 25 + $x + 2]->getSides()[2] ?? null;
-                $bottomOppositeSide = $y === 19 ? null : ($pieces[($y + 1) * 25 + $x + 2]->getSides()[0] ?? null);
-
-                $leftSide = $pieces[$y * 25 + $x + 2]->getSides()[1] ?? null;
-                $leftOppositeSide = $x === 0 ? null : ($pieces[$y * 25 + $x + 1]->getSides()[3] ?? null);
-
-                $topSide = $pieces[$y * 25 + $x + 2]->getSides()[0] ?? null;
-                $topOppositeSide = $y === 0 ? null : ($pieces[($y - 1) * 25 + $x + 2]->getSides()[2] ?? null);
-
-                $this->outputSideMatching($topSide, $topOppositeSide, $x . '/' . $y . ' (top)');
-                $this->outputSideMatching($leftSide, $leftOppositeSide, $x . '/' . $y . ' (left)');
-                $this->outputSideMatching($bottomSide, $bottomOppositeSide, $x . '/' . $y . ' (bottom)');
-                $this->outputSideMatching($rightSide, $rightOppositeSide, $x . '/' . $y . ' (right)');
-            }
-        }
-    }
-
-    private function outputSideMatching(?Side $side1, ?Side $side2, string $label): void
-    {
-        if ($side1 === null || $side2 === null) {
-            return;
-        }
-
-        echo $label . ': ' . round($this->service->getMatchingProbabilities($side1, [$side2])[0], 1) . PHP_EOL;
-    }
-
     public function testGetMatchingProbabilities(): void
     {
         $pieces = $this->getPieces();
@@ -70,17 +33,17 @@ class WeightedMatcherTest extends TestCase
         $matchingPositionsSum = 0;
         for ($x = 0; $x < 25; ++$x) {
             for ($y = 0; $y < 20; ++$y) {
-                $rightSide = $pieces[$y * 25 + $x + 2]->getSides()[3] ?? null;
-                $rightOppositeSide = $x === 24 ? null : ($pieces[$y * 25 + $x + 3]->getSides()[1] ?? null);
+                $rightSide = $this->getSide($pieces, $y * 25 + $x + 2, 3);
+                $rightOppositeSide = $x === 24 ? null : ($this->getSide($pieces, $y * 25 + $x + 3, 1));
 
-                $bottomSide = $pieces[$y * 25 + $x + 2]->getSides()[2] ?? null;
-                $bottomOppositeSide = $y === 19 ? null : ($pieces[($y + 1) * 25 + $x + 2]->getSides()[0] ?? null);
+                $bottomSide = $this->getSide($pieces, $y * 25 + $x + 2, 2);
+                $bottomOppositeSide = $y === 19 ? null : ($this->getSide($pieces, ($y + 1) * 25 + $x + 2, 0));
 
-                $leftSide = $pieces[$y * 25 + $x + 2]->getSides()[1] ?? null;
-                $leftOppositeSide = $x === 0 ? null : ($pieces[$y * 25 + $x + 1]->getSides()[3] ?? null);
+                $leftSide = $this->getSide($pieces, $y * 25 + $x + 2, 1);
+                $leftOppositeSide = $x === 0 ? null : ($this->getSide($pieces, $y * 25 + $x + 1, 3));
 
-                $topSide = $pieces[$y * 25 + $x + 2]->getSides()[0] ?? null;
-                $topOppositeSide = $y === 0 ? null : ($pieces[($y - 1) * 25 + $x + 2]->getSides()[2] ?? null);
+                $topSide = $this->getSide($pieces, $y * 25 + $x + 2, 0);
+                $topOppositeSide = $y === 0 ? null : ($this->getSide($pieces, ($y - 1) * 25 + $x + 2, 2));
 
                 $this->outputSideMatchings($topSide, $topOppositeSide, $allSides, $x . '/' . $y . ' (top)', $countMatchings, $matchingPositionsSum);
                 $this->outputSideMatchings($leftSide, $leftOppositeSide, $allSides, $x . '/' . $y . ' (left)', $countMatchings, $matchingPositionsSum);
@@ -89,10 +52,27 @@ class WeightedMatcherTest extends TestCase
             }
         }
 
-        echo "Average position: " . ($matchingPositionsSum / $countMatchings) . PHP_EOL;
+        echo 'Current average position: ' . ($matchingPositionsSum / $countMatchings) . ' (last known average position: 3.6840)' . PHP_EOL;
+
+        $this->assertLessThanOrEqual(3.7, $matchingPositionsSum / $countMatchings);
     }
 
-    private function outputSideMatchings(?Side $side1, ?Side $side2, array $sides, string $label, &$countMatchings, &$matchingPositionsSum): void
+    /**
+     * @param Piece[] $pieces
+     */
+    private function getSide(array $pieces, int $pieceIndex, int $sideIndex): ?Side
+    {
+        if (!isset($pieces[$pieceIndex])) {
+            return null;
+        }
+
+        return $pieces[$pieceIndex]->getSides()[$sideIndex] ?? null;
+    }
+
+    /**
+     * @param Side[]     $sides
+     */
+    private function outputSideMatchings(?Side $side1, ?Side $side2, array $sides, string $label, int &$countMatchings, int &$matchingPositionsSum): void
     {
         if ($side1 === null || $side2 === null) {
             return;
@@ -103,7 +83,7 @@ class WeightedMatcherTest extends TestCase
         $targetIndex = array_search($side2, $sides);
         $position = array_search($targetIndex, array_keys($probabilities));
 
-        $countMatchings++;
+        ++$countMatchings;
         $matchingPositionsSum += $position;
 
         echo $label . ': #' . ($position + 1) . ' (' . implode(', ', array_map(fn (float $num): float => round($num, 2), array_slice($probabilities, 0, 10))) . ($position > 9 ? ', ..., ' . round($probabilities[$targetIndex], 2) : '') . ')' . PHP_EOL;
