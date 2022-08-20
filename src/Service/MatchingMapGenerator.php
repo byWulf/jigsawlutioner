97e3d8c373570241d73d7a55df52dafa3de8d427
byWulf <wulf1337@gmail.com>
2022-08-20 17:51:32 +0200

    Add parallelization for MatchingMapGenerator (500 pieces from 30s to 5s, 2000 pieces from 430s to 166s)

--- src/Service/MatchingMapGenerator.php
+++ src/Service/MatchingMapGenerator.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Bywulf\Jigsawlutioner\Service;
 
+use Amp\Parallel\Worker\DefaultPool;
 use Bywulf\Jigsawlutioner\Dto\Piece;
 use Bywulf\Jigsawlutioner\Service\SideMatcher\SideMatcherInterface;
+use Bywulf\Jigsawlutioner\Task\MatchingMapGeneratorTask;
+use function Amp\call;
+use function Amp\Promise\all;
+use function Amp\Promise\wait;
 
 class MatchingMapGenerator
 {
     public function __construct(
-        private SideMatcherInterface $sideMatcher
+        private readonly SideMatcherInterface $sideMatcher,
+        private readonly int $parallelTasks = 10,
     ) {
     }
 
@@ -21,7 +27,10 @@ class MatchingMapGenerator
      */
     public function getMatchingMap(array $pieces): array
     {
-        $matchingMap = [];
+        $piecesCount = count($pieces);
+        if ($piecesCount === 0) {
+            return [];
+        }
 
         $allSides = [];
         foreach ($pieces as $piece) {
@@ -30,20 +39,17 @@ class MatchingMapGenerator
             }
         }
 
-        foreach ($pieces as $piece) {
-            foreach ($piece->getSides() as $sideIndex => $side) {
-                $probabilities = $this->sideMatcher->getMatchingProbabilities($side, $allSides);
-                arsort($probabilities);
-                $matchingMap[$this->getKey($piece->getIndex(), $sideIndex)] = $probabilities;
+        $pool = new DefaultPool($this->parallelTasks);
 
-                // Remove own sides from map, because the puzzle must not be matched with itself
-                for ($i = 0; $i < 4; ++$i) {
-                    unset($matchingMap[$this->getKey($piece->getIndex(), $sideIndex)][$this->getKey($piece->getIndex(), $i)]);
-                }
-            }
+        $coroutines = [];
+        foreach (array_chunk($pieces, 50) as $piecesBatch) {
+            $coroutines[] = call(fn() => yield $pool->enqueue(new MatchingMapGeneratorTask($this->sideMatcher, $piecesBatch, $allSides)));
         }
 
-        return $matchingMap;
+        /** @var array<int, float[][]> $results */
+        $results = wait(all($coroutines));
+
+        return array_merge(...$results);
     }
 
     private function getKey(int $pieceNumber, int $sideIndex): string
